0c60852f852dd1b4a92aa42e56f585088c70d707
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-06-04 19:46:40 -0300

    sentry

--- WS.WEB/wwwroot/js/sentry.js
+++ WS.WEB/wwwroot/js/sentry.js
@@ -19,28 +19,30 @@ const ignoredErrors = [
 
 const version = appVersion?.trim() ? appVersion : "error";
 
-Sentry.init({
-    dsn: servicesConfig.SentryDsn,
-    SendDefaultPii: true, // enable ip
-    release: `ws-js@${version}`,
-    environment: env,
-    beforeSend(event) {
-        const message = event.exception?.values?.[0]?.value || event.message || "";
+window.sentryOnLoad = function () {
+    Sentry.init({
+        dsn: servicesConfig.SentryDsn,
+        SendDefaultPii: true, // enable ip
+        release: `ws-js@${version}`,
+        environment: env,
+        beforeSend(event) {
+            const message = event.exception?.values?.[0]?.value || event.message || "";
 
-        if (message && ignoredErrors.some(err => err.test(message))) {
-            return null;
-        }
+            if (message && ignoredErrors.some(err => err.test(message))) {
+                return null;
+            }
 
-        event.tags = {
-            "custom.version": version,
-            "custom.platform": storage.getLocalStorage("platform") ?? "error",
-        };
-        event.extra = {
-            browser_name: environment.getBrowserName() ?? "error",
-            browser_version: environment.getBrowserVersion() ?? "error",
-            operation_system: environment.getOperatingSystem() ?? "error",
-        };
+            event.tags = {
+                "custom.version": version,
+                "custom.platform": storage.getLocalStorage("platform") ?? "error",
+            };
+            event.extra = {
+                browser_name: environment.getBrowserName() ?? "error",
+                browser_version: environment.getBrowserVersion() ?? "error",
+                operation_system: environment.getOperatingSystem() ?? "error",
+            };
 
-        return event;
-    },
-});
+            return event;
+        },
+    });
+}
